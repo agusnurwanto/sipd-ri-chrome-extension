27f25dc77fc7a8ffadd66aad81a9b33f5c9f9b5c
Saiful Rahman <saifulbangil@gmail.com>
2023-04-05 03:03:48 +0700

    cek duplikat standar harga

--- js/content/app.js
+++ js/content/app.js
@@ -588,7 +588,7 @@ function cekUrl(current_url, nomor=1){
 						var simpan_multiaddkompakun = ''
 							+'<button type="button" class="btn btn-danger" name="simpan_multiaddkompakun" id="simpan_multiaddkompakun">Simpan</button>';
 						jQuery('app-ssh-form-akun .card-footer').prepend(simpan_multiaddkompakun);
-						run_script("jQuery('.ngb-modal-window > div > div > app-ssh-form-akun').modal('show')').on('hidden.bs.modal', function () {"
+						run_script("jQuery('.ngb-modal-window .app-ssh-form-akun').modal('show')').on('hidden.bs.modal', function () {"
 							+"jQuery('#simpan_addkompakun').show();"
 							+"jQuery('#simpan_multiaddkompakun').hide();"
 							+"jQuery('select[name=kompakun]').val('').trigger('change');"
@@ -674,6 +674,14 @@ function cekUrl(current_url, nomor=1){
 								+'<button style="margin-left: 20px;" class="btn btn-sm btn-danger" id="cek_duplikat_ssh">Cek Duplikat '+type_data_ssh+'</button>'
 							+'</div>';
 						jQuery('.page-title').append(btn);
+						var btn2 = ''
+							+'<div class="aksi-extension">'
+								+'<button style="margin-left: 20px;" class="btn btn-sm btn-danger" id="cek_duplikat_ssh">Cek Duplikat '+type_data_ssh+'</button>'
+								//+'<button style="margin-left: 20px;" class="btn btn-sm btn-success" id="set_mulit_rek">Set Multi Kode SH dan Rek. Belanja '+type_data_ssh+'</button>'
+								// +'<button style="margin-left: 20px;" class="btn btn-sm btn-info" id="show_id_ssh">Tampilkan ID Standar Harga</button>'
+								// +'<button style="margin-left: 20px;" class="btn btn-sm btn-primary" id="show_akun_ssh">Tampilkan Link Akun</button>'
+							+'</div>';
+						jQuery('#aksi-admin').append(btn2);	
 						jQuery('#singkron_ssh_ke_lokal').on('click', function(){
 							singkron_ssh_ke_lokal(type_data_ssh);
 						});
@@ -762,6 +770,14 @@ function cekUrl(current_url, nomor=1){
 								+'<button style="margin-left: 20px;" class="btn btn-sm btn-danger" id="cek_duplikat_ssh">Cek Duplikat '+type_data_ssh+'</button>'
 							+'</div>';
 						jQuery('.page-title').append(btn);
+						var btn2 = ''
+							+'<div class="aksi-extension">'
+								+'<button style="margin-left: 20px;" class="btn btn-sm btn-danger" id="cek_duplikat_ssh">Cek Duplikat '+type_data_ssh+'</button>'
+								//+'<button style="margin-left: 20px;" class="btn btn-sm btn-success" id="set_mulit_rek">Set Multi Kode SH dan Rek. Belanja '+type_data_ssh+'</button>'
+								// +'<button style="margin-left: 20px;" class="btn btn-sm btn-info" id="show_id_ssh">Tampilkan ID Standar Harga</button>'
+								// +'<button style="margin-left: 20px;" class="btn btn-sm btn-primary" id="show_akun_ssh">Tampilkan Link Akun</button>'
+							+'</div>';
+						jQuery('#aksi-admin').append(btn2);	
 						jQuery('#singkron_ssh_ke_lokal').on('click', function(){
 							singkron_ssh_ke_lokal(type_data_ssh);
 						});
@@ -851,6 +867,14 @@ function cekUrl(current_url, nomor=1){
 								+'<button style="margin-left: 20px;" class="btn btn-sm btn-danger" id="cek_duplikat_ssh">Cek Duplikat '+type_data_ssh+'</button>'
 							+'</div>';
 						jQuery('.page-title').append(btn);
+						var btn2 = ''
+							+'<div class="aksi-extension">'
+								+'<button style="margin-left: 20px;" class="btn btn-sm btn-danger" id="cek_duplikat_ssh">Cek Duplikat '+type_data_ssh+'</button>'
+								//+'<button style="margin-left: 20px;" class="btn btn-sm btn-success" id="set_mulit_rek">Set Multi Kode SH dan Rek. Belanja '+type_data_ssh+'</button>'
+								// +'<button style="margin-left: 20px;" class="btn btn-sm btn-info" id="show_id_ssh">Tampilkan ID Standar Harga</button>'
+								// +'<button style="margin-left: 20px;" class="btn btn-sm btn-primary" id="show_akun_ssh">Tampilkan Link Akun</button>'
+							+'</div>';
+						jQuery('#aksi-admin').append(btn2);	
 						jQuery('#singkron_ssh_ke_lokal').on('click', function(){
 							singkron_ssh_ke_lokal(type_data_ssh);
 						});
