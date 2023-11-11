39ac74afdba8be80d0dc2c1d4420345011978a4a
Agus Nurwanto <agusnurwantomuslim@gmail.com>
2023-11-11 08:46:02 +0700

    perbaikan singkron pengaturan sipd

--- js/content/master/pengaturan_sipd.js
+++ js/content/master/pengaturan_sipd.js
@@ -27,33 +27,35 @@ function singkron_pengaturan_sipd_lokal(){
                                 tahun_anggaran: _token.tahun,
                                 api_key: config.api_key,
                                 data: {
-                                        id_daerah : data.data[0].id_daerah,
-                                        // daerah: jQuery('h4.text-white.font-bold').text(),
-                                        kepala_daerah : data.data[0].kepala_daerah,
-                                        wakil_kepala_daerah : data.data[0].wakil_kepala_daerah,
-                                        awal_rpjmd: data.data[0].rpjmd_awal,
-                                        akhir_rpjmd: data.data[0].rpjmd_akhir,
-                                        set_kpa_sekda: data.data[0].set_kpa_sub_sekda, 
-                                        set_kpa_sub_sekda : data.data[0].set_kpa_sub_sekda, //baru
-                                        id_setup_anggaran: data.data[0].id_setup_anggaran, //baru
-                                        set_rkpd: data.data[0].set_rkpd, //baru
-                                        set_kua: data.data[0].set_kua, //baru
-                                        set_apbd: data.data[0].set_apbd, //baru
-                                        ref_program: data.data[0].ref_program, //baru
-                                        ref_giat: data.data[0].ref_giat, //baru
-                                        ref_akun: data.data[0].ref_akun, //baru
-                                        ref_skpd: data.data[0].ref_skpd, //baru
-                                        ref_sumber_dana: data.data[0].ref_sumber_dana, //baru
-                                        ref_lokasi: data.data[0].ref_lokasi, //baru
-                                        ref_standar_harga: data.data[0].ref_standar_harga, //baru
-                                        is_locked: data.data[0].is_locked, //baru
-                                        jenis_set_pagu: data.data[0].jenis_set_pagu, //baru
-                                        tahun_aksi: data.data[0].tahun_aksi, //baru
-                                        status_kd: data.data[0].status_kd, //baru
-                                        // pelaksana_rkpd: jQuery('select[name="pelaksana_rkpd"]').val(),
-                                        // pelaksana_kua: jQuery('select[name="pelaksana_kua"]').val(),
-                                        // pelaksana_apbd: jQuery('select[name="pelaksana_apbd"]').val(),
-                                        // set_kpa_sekda: jQuery('select[name="set_kpa_sekda"]').val(),
+                                    id_kab : _token.daerah_id,
+                                    id_prop : _token.id_prop,
+                                    is_prop : _token.is_prop,
+                                    id_daerah : data.data[0].id_daerah,
+                                    daerah: _token.daerah_nama,
+                                    kepala_daerah : data.data[0].kepala_daerah,
+                                    wakil_kepala_daerah : data.data[0].wakil_kepala_daerah,
+                                    awal_rpjmd: data.data[0].rpjmd_awal,
+                                    akhir_rpjmd: data.data[0].rpjmd_akhir,
+                                    set_kpa_sekda: data.data[0].set_kpa_sub_sekda, 
+                                    set_kpa_sub_sekda : data.data[0].set_kpa_sub_sekda,
+                                    id_setup_anggaran: data.data[0].id_setup_anggaran,
+                                    set_rkpd: data.data[0].set_rkpd,
+                                    set_kua: data.data[0].set_kua,
+                                    set_apbd: data.data[0].set_apbd,
+                                    ref_program: data.data[0].ref_program,
+                                    ref_giat: data.data[0].ref_giat,
+                                    ref_akun: data.data[0].ref_akun,
+                                    ref_skpd: data.data[0].ref_skpd,
+                                    ref_sumber_dana: data.data[0].ref_sumber_dana,
+                                    ref_lokasi: data.data[0].ref_lokasi,
+                                    ref_standar_harga: data.data[0].ref_standar_harga,
+                                    is_locked: data.data[0].is_locked,
+                                    jenis_set_pagu: data.data[0].jenis_set_pagu,
+                                    tahun_aksi: data.data[0].tahun_aksi,
+                                    status_kd: data.data[0].status_kd,
+                                    pelaksana_rkpd: jQuery('#set_rkpd option:selected').text(),
+                                    pelaksana_kua: jQuery('#set_kua option:selected').text(),
+                                    pelaksana_apbd: jQuery('#set_apbd option:selected').text()
                                 }
                             },
                             return: true
